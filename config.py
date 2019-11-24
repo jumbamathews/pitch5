72686c55947309cb749d428e2fbbae0176a3b546
MATHEWS_JUMBA <mathewsjumba19@outlook.com>
2019-11-24 21:07:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://admin:DadMomLove@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI='postgres://bqljixmurkwqbq:5ab69c1da07af3ac3ff3288776cb4dabf415d2cc26cc2ef8c611bc18e2ca6ec7@ec2-107-22-239-155.compute-1.amazonaws.com:5432/dc7ckrjl8pdlec'
     DEBUG = True
 
 
