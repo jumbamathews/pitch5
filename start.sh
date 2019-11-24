db1e3e007bb102fb1d03028e29370bade1adc0ca
MATHEWS_JUMBA <mathewsjumba19@outlook.com>
2019-11-24 21:09:11 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgres://xkoszvcfcdlkrk:9d454a4207418f7d6084c0af1fa0c93a32656ec9783bef5ec6c86212ba57132a@ec2-54-235-240-126.compute-1.amazonaws.com:5432/d18732jk577r6a'
+export DATABASE_URL='postgres://bqljixmurkwqbq:5ab69c1da07af3ac3ff3288776cb4dabf415d2cc26cc2ef8c611bc18e2ca6ec7@ec2-107-22-239-155.compute-1.amazonaws.com:5432/dc7ckrjl8pdlec'
 
 export SECRET_KEY='power3000'
 
